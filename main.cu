0de1cc875222fcc69141fa1ebe92072573af9eba
cpaca <37684627+cpaca@users.noreply.github.com>
2023-05-05 19:54:53 -0700

    Fix copyBundlesToDevice memory accessing

--- main.cu
+++ main.cu
@@ -126,66 +126,17 @@ void convertArrToCuda(size_t* &arr, size_t arrSize){
 }
 
 /**
- * Converts bundle data into a 1D array created using cudaMalloc
- * @param bundleData Bundle data. Each element in the array is a bundle, represented by another array terminated by -1.
- *  Note that each bundle need not be the same size. (Hence the -1 termination.)
- * @param numBundles The number of bundles.
- * @param[out] deviceBundles Output: The bundleData, represented as a 1D array. Basically, a flattened version of bundleData.
- * @param[out] bundleIndices The index (in bundleSeries) of each bundle. Note that bundleIndices[0] = 0 since the first
- *  bundle will obviously start at the first index of bundleSeries.
+ * Copies a size_t array in host memory to a size_t array in device memory. Performs the cudaMalloc for you.
+ * Calls delete[] on the host memory for you, though you have to cudaFree on your own.
+ * Or let the application exit to clean up the memory.
+ * @param to A size_t array in host memory.
+ * @param from A size_t array in device memory.
+ * @param arrSize The size of each array.
  */
-void copyBundlesToDevice(size_t** bundleData, size_t numBundles, size_t* &deviceBundles, size_t* &bundleIndices){
-    // First, we need to convert bundleData into a 1D array.
-    // In other words, we need to flatten it.
-    // First, find out how big the flattened array will be.
-    // While we're doing this, we can also find out the indices of each bundle.
-    size_t flattenedSize = 0;
-    bundleIndices = new size_t[numBundles];
-    for(size_t bundleNum = 0; bundleNum < numBundles; bundleNum++){
-        // The start of this bundle is at index flattenedSize
-        bundleIndices[bundleNum] = flattenedSize;
-
-        size_t bundleIdx = 0;
-        while(bundleData[bundleNum][bundleIdx] != -1){
-            // not the end
-            // so we need to add this to the flattened list
-            flattenedSize++;
-            // and check if the next one is the end
-            bundleIdx++;
-        }
-        // and one more for the -1 position.
-        flattenedSize++;
-        bundleIdx++;
-    }
-
-    // Now we can construct our 1D array
-    deviceBundles = new size_t[flattenedSize];
-    size_t deviceBundleIdx = 0;
-    // Copy each array over to their respective position...
-    // Normally I'd just figure out memcpy but, well, no trust.
-    // Also I haven't formally been taught memcpy so I don't trust my knowledge of it.
-    for(size_t bundleNum = 0; bundleNum < numBundles; bundleNum++){
-        if(bundleIndices[bundleNum] != deviceBundleIdx){
-            // this bundle isn't starting in the right spot.
-            throw std::logic_error("A bundle isn't starting in the right spot.");
-        }
-        size_t bundleIdx = 0;
-        while(bundleData[bundleNum][bundleIdx] != -1){
-            deviceBundles[deviceBundleIdx] = bundleData[bundleNum][bundleIdx];
-            deviceBundleIdx++;
-            bundleIdx++;
-        }
-        // and then copy over the -1
-        deviceBundles[deviceBundleIdx] = -1;
-        deviceBundleIdx++;
-    }
-    if(deviceBundleIdx != flattenedSize){
-        throw std::logic_error("The flattened list isn't the right size.");
-    }
-
-    // Finally, CUDAify bundleSeries and bundleIndices
-    convertArrToCuda(bundleIndices, numBundles);
-    convertArrToCuda(deviceBundles, flattenedSize);
+void copyArrToCuda(size_t* &to, size_t* from, size_t arrSize){
+    cudaMalloc(&to, arrSize * sizeof(size_t));
+    cudaMemcpy(&to, from, arrSize * sizeof(size_t), cudaMemcpyHostToDevice);
+    delete[] from;
 }
 
 /**
@@ -333,25 +284,26 @@ __device__ size_t* bundleSeries = nullptr;
 // Index of each bundle in bundleSeries. So bundleSeries[bundleIndices[n]] is the first index of a bundle in bundleSeries.
 __device__ size_t* bundleIndices = nullptr;
 
-// For each set, what bundles contain it?
-// The format is... kind of a long description.
-// First, let setBundlesSetSize = (numBundles/sizeof(size_t))
-// And for shorthand, let sBSS = setBundlesSetSize
-// Indices setBundles[setNum * sBSS] to setBundles[(setNum+1)*sBSS - 1] are the indices for set setNum
-// In other words, to loop over all values in setBundles relevant to a set:
-// for(int i = 0; i < sBSS; i++){/*do something with setBundles[setNum*sBSS + i]*/}
-//
-// Now express setBundles[0], setBundles[1], ... as a bitstream.
-// The first bit represents if the set is in bundle # 0
-// The second bit represents if the set is in bundle # 1
-// etc.
-// Because this is a bitstream and size_t is 64-bits:
-// the 65th bit (aka, the first bit of setBundles[1], aka setBundles[1]&0) represents if the set is in bundle #65
-//
-// Note that this is setBundles, so it needs to work for all SETS. Even Bundles.
-// Also note that for bundles, their "bitstream" is all 0s except for itself, where it is 1.
-// TODO: Optimization: Bundles which are entirely contained within other bundles (ex cover corp is in vtubers)
-//  could have a better setBundles value.
+/* For each set, what bundles contain it?
+ * The format is... kind of a long description.
+ * First, let setBundlesSetSize = (numBundles/sizeof(size_t))
+ * And for shorthand, let sBSS = setBundlesSetSize
+ * Indices setBundles[setNum * sBSS] to setBundles[(setNum+1)*sBSS - 1] are the indices for set setNum
+ * In other words, to loop over all values in setBundles relevant to a set:
+ * for(int i = 0; i < sBSS; i++){ do something with setBundles[setNum*sBSS + i]}
+ *
+ * Now express setBundles[0], setBundles[1], ... as a bitstream.
+ * The first bit represents if the set is in bundle # 0
+ * The second bit represents if the set is in bundle # 1
+ * etc.
+ * Because this is a bitstream and size_t is 64-bits:
+ * the 65th bit (aka, the first bit of setBundles[1], aka setBundles[1]&0) represents if the set is in bundle #65
+ *
+ * Note that this is setBundles, so it needs to work for all SETS. Even Bundles.
+ * Also note that for bundles, their "bitstream" is all 0s except for itself, where it is 1.
+ * TODO: Optimization: Bundles which are entirely contained within other bundles (ex cover corp is in vtubers)
+ *  could have a better setBundles value.
+ */
 __device__ size_t* setBundles = nullptr;
 __device__ size_t setBundlesSetSize = -1; // note that setBundles[-1] = illegal (unsigned type)
 
@@ -363,6 +315,69 @@ __device__ size_t* deviceSeries = nullptr;
 // Free bundles.
 // If freeBundles[n] is non-zero, then bundle n is free.
 __device__ size_t* freeBundles = nullptr;
+
+/**
+ * Converts bundle data into a 1D array created using cudaMalloc
+ * @param bundleData Bundle data. Each element in the array is a bundle, represented by another array terminated by -1.
+ *  Note that each bundle need not be the same size. (Hence the -1 termination.)
+ * @param numBundles The number of bundles.
+ * @param[out] deviceBundles Output: The bundleData, represented as a 1D array. Basically, a flattened version of bundleData.
+ * @param[out] deviceIndices The index (in bundleSeries) of each bundle. Note that deviceIndices[0] = 0 since the first
+ *  bundle will obviously start at the first index of bundleSeries.
+ */
+void copyBundlesToDevice(size_t** bundleData, size_t numBundles){
+    // First, we need to convert bundleData into a 1D array.
+    // In other words, we need to flatten it.
+    // First, find out how big the flattened array will be.
+    // While we're doing this, we can also find out the indices of each bundle.
+    size_t flattenedSize = 0;
+    auto* hostIndices = new size_t[numBundles];
+    for(size_t bundleNum = 0; bundleNum < numBundles; bundleNum++){
+        // The start of this bundle is at index flattenedSize
+        hostIndices[bundleNum] = flattenedSize;
+
+        size_t bundleIdx = 0;
+        while(bundleData[bundleNum][bundleIdx] != -1){
+            // not the end
+            // so we need to add this to the flattened list
+            flattenedSize++;
+            // and check if the next one is the end
+            bundleIdx++;
+        }
+        // and one more for the -1 position.
+        flattenedSize++;
+        bundleIdx++;
+    }
+
+    // Now we can construct our 1D array
+    auto* hostBundles = new size_t[flattenedSize];
+    size_t deviceBundleIdx = 0;
+    // Copy each array over to their respective position...
+    // Normally I'd just figure out memcpy but, well, no trust.
+    // Also I haven't formally been taught memcpy so I don't trust my knowledge of it.
+    for(size_t bundleNum = 0; bundleNum < numBundles; bundleNum++){
+        if(hostIndices[bundleNum] != deviceBundleIdx){
+            // this bundle isn't starting in the right spot.
+            throw std::logic_error("A bundle isn't starting in the right spot.");
+        }
+        size_t bundleIdx = 0;
+        while(bundleData[bundleNum][bundleIdx] != -1){
+            hostBundles[deviceBundleIdx] = bundleData[bundleNum][bundleIdx];
+            deviceBundleIdx++;
+            bundleIdx++;
+        }
+        // and then copy over the -1
+        hostBundles[deviceBundleIdx] = -1;
+        deviceBundleIdx++;
+    }
+    if(deviceBundleIdx != flattenedSize){
+        throw std::logic_error("The flattened list isn't the right size.");
+    }
+
+    // Finally, CUDAify bundleSeries and deviceIndices
+    copyArrToCuda(bundleIndices, hostIndices, numBundles);
+    copyArrToCuda(bundleSeries, hostBundles, flattenedSize);
+}
 
 __device__ size_t getSetSize(size_t setNum, size_t numSeries){
     if(setNum < numSeries){
@@ -814,12 +829,7 @@ int main() {
     // Can't do that for bundleData because bundleData is non-rectangular.
     // ... bleh, cudaMallocPitch is annoying, I might also do seriesData as a 1D array...
 
-    bundleSeries = nullptr;
-    bundleIndices = nullptr;
-    copyBundlesToDevice(bundleData, numBundles, bundleSeries, bundleIndices);
-    if(bundleSeries == nullptr || bundleIndices == nullptr){
-        throw std::logic_error("Device bundles and/or bundleIndices did not get overwritten properly.");
-    }
+    copyBundlesToDevice(bundleData, numBundles);
 
     deviceSeries = nullptr;
     copySeriesToDevice(seriesData, numSeries, deviceSeries);
