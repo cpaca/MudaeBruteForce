c48896315634e650a73c37c3e132d494e88b01d2
cpaca <37684627+cpaca@users.noreply.github.com>
2023-06-07 15:55:25 -0700

    Create Task struct

--- task.cu
+++ task.cu
@@ -1,4 +1,9 @@
 #ifndef MUDAEBRUTEFORCE_TASK
 #define MUDAEBRUTEFORCE_TASK
-
+struct {
+    size_t* disabledSets; // List of disabled sets
+    // disabledSets[0] to disabledSets[index-1] are defined
+    // and disabledSets[index] onwards are undefined
+    size_t disabledSetsIndex;
+} Task;
 #endif
