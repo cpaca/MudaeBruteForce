4e224ea1a7aa4b9f0782041374c6d64b0b3150de
cpaca <37684627+cpaca@users.noreply.github.com>
2023-06-13 13:52:20 -0700

    Implement copy constructor

--- task.cu
+++ task.cu
@@ -14,4 +14,27 @@ typedef struct {
     // What the score was the last time it was calculated for this Task
     size_t score;
 } Task;
+
+/**
+ * Returns a new Task that is identical to the given Task
+ * Basically, a copy constructor.
+ */
+__host__ __device__ Task* copyTask(Task* task){
+    if(task == nullptr){
+        return nullptr;
+    }
+    size_t disabledSetsSize = MAX_DL + MAX_FREE_BUNDLES;
+    Task* newTask = new Task;
+    newTask->disabledSets = new size_t[disabledSetsSize];
+    newTask->disabledSetsIndex = task->disabledSetsIndex;
+    for(size_t i = 0; i < newTask->disabledSetsIndex; i++){
+        newTask[i] = task[i];
+    }
+
+    newTask->setDeleteIndex = task->setDeleteIndex;
+    newTask->shouldDeleteNext = task->shouldDeleteNext;
+    newTask->score = task->score;
+
+    return newTask;
+}
 #endif
