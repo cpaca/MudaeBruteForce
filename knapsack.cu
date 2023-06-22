3c77326655dfe4964677c218bfc7bc8743746734
cpaca <37684627+cpaca@users.noreply.github.com>
2023-06-22 16:31:34 -0700

    Attempt to write knapsackWrite?

--- knapsack.cu
+++ knapsack.cu
@@ -15,6 +15,7 @@ __host__ void knapsackInit(){
 
     char* host_newBestScores;
     cudaMallocPitch(&host_newBestScores, &host_pitch, fakeRowWidth, numRows);
+    std::cout << "host_pitch: " << std::to_string(host_pitch) << "\n";
     cudaMemset(host_newBestScores, 0, host_pitch * numRows);
     cudaMemcpyToSymbol(bestScoresPitch, &host_pitch, sizeof(size_t));
     cudaMemcpyToSymbol(newBestScores, &host_newBestScores, sizeof(char*));
@@ -51,14 +52,57 @@ __device__ bool knapsackIsTaskGood(Task* task){
 
 __device__ void knapsackWrite(const size_t &DLSlotsUsed, const size_t &overlapUsed, const size_t &score){
     size_t rowNum = DLSlotsUsed;
-    size_t colNum = overlapUsed;
+    size_t resetCol = overlapUsed;
 
     char* rowAddress = newBestScores + (rowNum * bestScoresPitch);
     auto* row = (size_t*) rowAddress;
-    size_t* col = row + colNum;
+    size_t* col = row + resetCol;
 
-    const size_t oldScore = atomicMax(col, score);
-    // TODO propagate down the newBestScores knapsack
+    bool justReset = true;
+    size_t currCol = resetCol;
+
+//    size_t oldScore = atomicMax(col, score);
+    while(rowNum <= MAX_DL){
+        size_t oldScore = atomicMax(col, score);
+
+        if((oldScore >= score) || (currCol > OVERLAP_LIMIT)){
+            // Attempt to go to the next row
+            if(justReset){
+                // okay, we just reset
+                // and the next row will make us reset too
+                // so just leave
+                return;
+            }
+            else{
+                // Go to the next row:
+                rowNum++;
+
+                // Reset the col:
+                rowAddress = newBestScores + (rowNum * bestScoresPitch);
+                row = (size_t*) rowAddress;
+                currCol = resetCol;
+                col = row + currCol;
+
+                justReset = true;
+            }
+        }
+        else{
+            // Go to the next column
+            currCol++;
+            col = row + currCol;
+
+            // this isn't a reset.
+            justReset = false;
+        }
+    }
+//    if(oldScore < score){
+//        if(DLSlotsUsed < MAX_DL) {
+//            knapsackWrite(DLSlotsUsed + 1, overlapUsed, score);
+//        }
+//        if(overlapUsed < OVERLAP_LIMIT) {
+//            knapsackWrite(DLSlotsUsed, overlapUsed + 1, score);
+//        }
+//    }
 }
 
 __device__ void knapsackWriteTask(Task* task){
